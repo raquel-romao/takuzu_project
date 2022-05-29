2acbe94de80b672c5c867e6dc8c2c27dfbf3747b
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-05-29 15:45:00 +0100

    board change que mostrei

--- takuzu.py
+++ takuzu.py
@@ -40,8 +40,9 @@ class Board:
     def __init__(self, board_size): 
         self.board = np.ones((board_size,board_size), dtype=object)*2 
         self.board_size = board_size
-        self.info = np.zeros((board_size * 2,3), dtype=object) #a minha ideia aqui e fazer um array de arrays com 3 instancias (numero de 0s, numero de 1s, lista de coordenadas vazias) e seria com tamanho o dobro do board_size para guardar info linhas + colunas
-# fiz esta cena do info porque pelo que estive a pesquisar a complexidade é O8n9 para contar o numero de instancias de algo um array, e como vamos er de ir busca bue vezes essa info achei por melhor 'gastar'mais tempo na construçºao da board para depois compensar on the long run
+        self.info = np.zeros((board_size * 2,2), dtype=object) 
+        
+    
     def __str__(self):
         for i in self.board:
             for j in i:
@@ -106,6 +107,7 @@ class Board:
         board_size = int(stdin.readline().rstrip('\n'))
         board = Board(board_size)
         
+        
         for i in range(board_size): #aqui começa no zero certo? -> yes!
             values = stdin.readline().rstrip('\n').split('\t') #retorna lista de strings com os numeros
             for j in range(len(values)):
@@ -113,28 +115,11 @@ class Board:
                 board.set_number(value,i,j)
                 if value == 1:
                     board.info[i][1] +=1
-                    board.info[board_size + j - 1][1] +=1                    
+                    board.info[board_size + j - 1][1] += 1                    
                 elif value == 0:
                     board.info[i][0]+=1
-                    board.info[board_size + j - 1][0] +=1  
-                else:
-                    row=board.info[i][2]
-                    col=board.info[board_size + j -1][2]
-                    #queria que a 3ª nstancia para cada  linha do array board_info fosse um array de ttuples cm as coordenadas dos que estavam vazios tno nas colunas como nas linhas mas nºao estou a consegguir que mantenha o formato tuple
-                    if row == 0:
-                        board.info[i][2] = np.array((i,j))
-                    else:
-                        board.info[i][2] = np.append(row,[(i,j)]) #bruh
-                    if col == 0:
-                        board.info[board_size + j -1][2] = np.array((i,j))
-                    else:
-                        board.info[board_size + j -1][2]=np.append(col,[(i,j)]) #bruh
-                    
-
-
-
-            
-            
+                    board.info[board_size + j - 1][0] += 1 
+               
         return board
 
     # TODO: outros metodos da classe
@@ -143,7 +128,7 @@ class Board:
 class Takuzu(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
-        # TODO
+        # 
         pass
 
     def actions(self, state: TakuzuState):
