7e2da7d1bf704b18407954dd0f561ffedaf10a84
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-01 16:47:23 +0100

    testes

--- takuzu.py
+++ takuzu.py
@@ -266,7 +266,12 @@ print(board)
 
 problem= Takuzu(board)
 
-goal_node=depth_first_tree_search(problem)
+#goal_node=depth_first_tree_search(problem)
 
-print("Is goal?", problem.goal_test(goal_node.state))
-print("Solution:\n", goal_node.state.board)
+new = problem.result(problem.state,(1,0,0))
+print(new.board)
+new = problem.result(new,(2,2,1))
+print(new.board)
+
+print("Is goal?", problem.goal_test(new))
+print("Solution:\n", new.board)
