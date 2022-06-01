5a78ffd7915339ba645fa644eb7b882ae1d616dc
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-01 18:43:46 +0100

    test

--- takuzu.py
+++ takuzu.py
@@ -219,11 +219,8 @@ class Takuzu(Problem):
         if board_size % 2 == 0:
             return np.all(sum_col==half) and np.all(sum_lines==half) 
         else:
-            col = (half-1 < sum_col <half+2)
+            col = (half < sum_col < half+2)
             lin = (half-1< sum_lines < half+2)
-            print(col)
-            print(type(col))
-            print(type(lin))
             return any(half<=sum_col<=half+1) and any(half<=sum_lines<=half+1)
 
     def adjacent(self, state: TakuzuState):
