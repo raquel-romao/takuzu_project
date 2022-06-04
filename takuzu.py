a6b18be217c3ae0cc7669f47132ed622dd268e28
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-04 13:02:57 +0100

    actions

--- takuzu.py
+++ takuzu.py
@@ -54,10 +54,9 @@ class TakuzuState:
             for i in empty:
                 if line[i[0]][0] < half and col[i[1]][0]< half:
                     actions.append((i[0],i[1],0))
-                elif line[i[0]][1] < half and col[i[1]][1] < half:
+                if line[i[0]][1] < half and col[i[1]][1] < half:
                     actions.append((i[0],i[1],1))
-                else:
-                    return []
+                    
             self.possible_actions = actions
         
         return self.possible_actions
