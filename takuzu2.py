e8c8db6f9396364f753d8bc595f623c9291455c4
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-17 17:14:48 +0100

    empty

--- takuzu2.py
+++ takuzu2.py
@@ -291,5 +291,5 @@ if __name__ == "__main__":
     # Obter o nó solução usando a procura em profundidade:
     goal_node = depth_first_tree_search(problem)
     # Verificar se foi atingida a solução
-    print("Is goal?", problem.goal_test(goal_node.state))
-    print("Solution:\n", goal_node.state.board)
+    #print("Is goal?", problem.goal_test(goal_node.state))
+    #print("Solution:\n", goal_node.state.board)
