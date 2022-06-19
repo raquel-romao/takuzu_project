ed9aa0fb213100196fa35b0210ce0286b3797878
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-19 14:52:47 +0100

    actions

--- takuzu2.py
+++ takuzu2.py
@@ -64,18 +64,6 @@ class TakuzuState:
                 position_actions.append((row_idx, col_idx, 1))
 
 
-            """
-
-            if line[i[0]][0] < half and col[i[1]][0] < half and \
-                self.board.adjacent_vertical_numbers(i[0],i[1]).count(0)!=2 and \
-                    self.board.adjacent_horizontal_numbers(i[0],i[1]).count(0)!=2:
-                position_actions.append((i[0],i[1],0))
-
-            if line[i[0]][1] < half and col[i[1]][1] < half and \
-                self.board.adjacent_vertical_numbers(i[0],i[1]).count(1)!=2 and \
-                    self.board.adjacent_horizontal_numbers(i[0],i[1]).count(1)!=2:
-                position_actions.append((i[0],i[1],1))
-            """
             for a in position_actions:
                 test_row = self.board.board[a[0]].copy()
                 test_row[a[1]] = a[2] 
