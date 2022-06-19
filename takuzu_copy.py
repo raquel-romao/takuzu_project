ac1023b19046725cf9cf65fae88da3c8e5c84bd3
raquel-romao <raquelromao4700@gmail.com>
2022-06-19 13:21:51 +0100

    updatessss

--- takuzu_copy.py
+++ takuzu_copy.py
@@ -3,9 +3,6 @@
 # 92759 Laura Quintas
 # 92780 Raquel Romão
 
-#import sys (como estava antes)
-
-
 from hashlib import new
 from sys import stdin
 import numpy as np
@@ -28,10 +25,9 @@ class TakuzuState:
         self.board = board
         self.id = TakuzuState.state_id
         TakuzuState.state_id += 1
-        #self.open = False
-        self.possible_actions = None
-        self.rows = set(["".join(str(x) for x in arr) for arr in board.board])
-        self.cols = set(["".join(str(x) for x in arr) for arr in board.board.transpose()])
+        self.open = False
+        self.rows = set(str(arr) for arr in board.board)
+        self.cols = set(str(arr) for arr in board.board.transpose())
 
 
     def __lt__(self, other):
@@ -43,94 +39,72 @@ class TakuzuState:
 
 
     def actions(self):
-        if self.possible_actions == None:
-            line = np.column_stack(((self.board.board==0).sum(axis=1), (self.board.board==1).sum(axis=1)))
-            col = np.column_stack(((self.board.board==0).sum(axis=0), (self.board.board==1).sum(axis=0)))
-            actions = []
-            empty = self.empty_positions()
+        #if not self.open: #então tiramos? -> e expand + à frente?
+        line = np.column_stack(((self.board.board==0).sum(axis=1), (self.board.board==1).sum(axis=1)))
+        col = np.column_stack(((self.board.board==0).sum(axis=0), (self.board.board==1).sum(axis=0)))
+        actions = []
+        empty = self.empty_positions()
 
-            if self.board.board_size % 2 == 0:
-                half = self.board.board_size //2
-            else:
-                half = self.board.board_size //2 + 1
+        if self.board.board_size % 2 == 0:
+            half = self.board.board_size //2
+        else:
+            half = self.board.board_size //2 + 1
 
-            for i in empty:
-                position_actions = []
+        for i in empty:
+            row_idx, col_idx = i
 
-                if line[i[0]][0] < half and col[i[1]][0] < half and self.board.adjacent_vertical_numbers(i[0],i[1]).count(0)!=2 and self.board.adjacent_horizontal_numbers(i[0],i[1]).count(0)!=2:
-                    position_actions.append((i[0],i[1],0))
+            position_actions = []
 
-                if line[i[0]][1] < half and col[i[1]][1] < half and self.board.adjacent_vertical_numbers(i[0],i[1]).count(1)!=2 and self.board.adjacent_horizontal_numbers(i[0],i[1]).count(1)!=2:
-                    position_actions.append((i[0],i[1],1))
+            if line[row_idx][0] < half and col[col_idx][0] < half and self.board.horizontal(row_idx, col_idx, 0) and self.board.vertical(row_idx, col_idx, 0):
+                position_actions.append((row_idx, col_idx, 0))
 
-                for a in position_actions:
-                    play = str(a[2])
-                    test_row = "".join(str(x) for x in self.board.board[a[0]])
-                    test_row = test_row[:a[1]] + play + test_row[a[1]+1:]
 
-                    test_col = "".join(str(x) for x in self.board.board[:, a[1]])
-                    test_col = test_col[:a[0]] + play + test_col[a[0]+1:]
+            if line[row_idx][1] < half and col[col_idx][1] < half and self.board.horizontal(row_idx, col_idx, 1) and self.board.vertical(row_idx, col_idx, 1):
+                position_actions.append((row_idx, col_idx, 1))
 
-                    if test_row in self.rows or test_col in self.cols:
-                        position_actions.remove(a)
 
-                if len(position_actions)==2:
-                    actions.append(position_actions[0])
-                    actions.append(position_actions[1])
+            """
 
-                elif len(position_actions)==1:
-                    a=position_actions[0]
-                    self.board.set_number(a[0],a[1],a[2])
+            if line[i[0]][0] < half and col[i[1]][0] < half and \
+                self.board.adjacent_vertical_numbers(i[0],i[1]).count(0)!=2 and \
+                    self.board.adjacent_horizontal_numbers(i[0],i[1]).count(0)!=2:
+                position_actions.append((i[0],i[1],0))
 
-                    if 2 not in self.board.board:
-                        actions.append(a)
+            if line[i[0]][1] < half and col[i[1]][1] < half and \
+                self.board.adjacent_vertical_numbers(i[0],i[1]).count(1)!=2 and \
+                    self.board.adjacent_horizontal_numbers(i[0],i[1]).count(1)!=2:
+                position_actions.append((i[0],i[1],1))
+            """
+            for a in position_actions:
+                test_row = self.board.board[a[0]].copy()
+                test_row[a[1]] = a[2] 
+                test_col = self.board.board[:,a[1]].copy()
+                test_col[a[0]] = a[2]
 
-                else:
-                    self.possible_actions = []
-                    return self.possible_actions
+                if str(test_row) in self.rows or str(test_col) in self.cols:
+                    position_actions.remove(a)
 
-            self.possible_actions = actions
+            if len(position_actions)==2:
+                actions.append(position_actions[0])
+                actions.append(position_actions[1])
 
-        return self.possible_actions
+            elif len(position_actions)==1:
+                a=position_actions[0]
+                self.board.set_number(*a)
+
+                if 2 not in self.board.board:
+                    actions.append(a)
+        
+        return actions
 
     def empty_positions(self):
         result = np.where(self.board.board == 2)
-        empty = np.column_stack((result[0],result[1]))
+        empty = np.column_stack(result)
         return empty
-
-    """def prioritize(self, actions): #para depois aplicar à actions a devolver
-        #board = self.board.board
-
-        #for a in actions:
-            
-                
-                if self.board.adjacent_vertical_numbers(a[0],a[1])==1 and self.board.get_number(a[0]-1, a[1])==1:
-                    #priotitize
-                elif self.board.get_number(a[0]+1, a[1])==1 and self.board.get_number(a[0]+2, a[1])==1:
-                    #prioritize
-
-                elif self.board.get_number(a[0], a[1]-2)==1 and self.board.get_number(a[0], a[1]-1)==1:
-                    #prioritize
-                elif self.board.get_number(a[0], a[1]+1)==1 and self.board.get_number(a[0], a[1]+2)==1:
-                    #prioritize
-            else:
-                if self.board.get_number(a[0]-2, a[1])==1 and self.board.get_number(a[0]-1, a[1])==1:
-                    #priotitize
-                elif self.board.get_number(a[0]+1, a[1])==1 and self.board.get_number(a[0]+2, a[1])==1:
-                    #prioritize
-
-                elif self.board.get_number(a[0], a[1]-2)==1 and self.board.get_number(a[0], a[1]-1)==1:
-                    #prioritize
-                elif self.board.get_number(a[0], a[1]+1)==1 and self.board.get_number(a[0], a[1]+2)==1:
-                    #prioritize
-
-                    isto ia dar merda com os indices, caguei
-                    """
-
-    #def expand(self):
-        #self.open = True
     
-
+    def expand(self):
+        self.open = True
+    
 
 class Board:
     """Representação interna de um tabuleiro de Takuzu.""" 
@@ -139,7 +113,7 @@ class Board:
         self.board = board
         self.board_size = board_size
         self.empty = empty
-        self.string = str(self.board.ravel())
+        self.string = str(self.board)
         
     
     def __str__(self):
@@ -150,40 +124,73 @@ class Board:
                     prettyprint += f'{i[j]}\n'
                 else:
                     prettyprint += f'{i[j]}\t'
-        return prettyprint
+        return prettyprint.rstrip('\n')
 
     def set_number(self, row: int, col: int, value): 
         self.board[row, col] = value
         self.string = str(self.board.ravel()) # atualiza o hash value.
         
-        
     def get_number(self, row: int, col: int) -> int:
         """Devolve o valor na respetiva posição do tabuleiro."""
         return self.board[row, col] 
 
+
+    def count(self, t: tuple, i: int):
+        return sum(x == i for x in t)
+
     def adjacent_vertical_numbers(self, row: int, col: int):
         """Devolve os valores imediatamente abaixo e acima,
         respectivamente."""
-        
+
         if row == 0:
-            return (None, self.get_number(row + 1, col))
+            return (self.get_number(row + 1, col),)
         
         elif row == self.board_size - 1:
-            return (self.get_number(row - 1, col), None)
+            return (self.get_number(row - 1, col),)
 
         else:
             return (self.get_number(row - 1, col), self.get_number(row + 1, col))
 
+
+
+    def horizontal(self, row: int, col: int, move: int):
+        
+        n = self.board_size
+        check = []
+
+        if (col not in (n-1, n-2)):
+            check.append((self.get_number(row, col+1), self.get_number(row, col+2))) #guardar array de posições contíguas
+        if (col not in (0, 1)):
+            check.append((self.get_number(row, col-1), self.get_number(row, col-2)))
+        if (col not in (0, n-1)):
+            check.append((self.get_number(row, col-1), self.get_number(row, col+1)))
+
+
+        return all(self.count(t, move) != 2 for t in check)
+
+
+    def vertical(self, row: int, col:int, move:int):
+        n = self.board_size
+        check = []
+
+        if (row not in (n-1, n-2)):
+            check.append((self.get_number(row+1, col), self.get_number(row+2, col)))
+        if (row not in (0, 1)):
+            check.append((self.get_number(row-1, col), self.get_number(row-2, col)))
+        if (row not in (0, n-1)):
+            check.append((self.get_number(row-1, col), self.get_number(row+1, col)))
+
+        return all(self.count(t, move) != 2 for t in check)
 
     def adjacent_horizontal_numbers(self, row: int, col: int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
       
         if col == 0:
-            return (None, self.get_number(row, col + 1))
+            return (self.get_number(row, col + 1),)
         
         elif col == self.board_size - 1:
-            return (self.get_number(row, col - 1), None)
+            return (self.get_number(row, col - 1),)
 
         else:
             return (self.get_number(row, col - 1), self.get_number(row, col + 1))
@@ -215,11 +222,7 @@ class Board:
                 if value == 2:
                     empty.append((i,j))
 
-
         new_board = Board(board, board_size, empty)
-
-
-
         return new_board
 
 
@@ -234,7 +237,7 @@ class Takuzu(Problem):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
         actions = state.actions()
-        #state.expand() #needed?
+        #state.expand()
         return actions
 
 
@@ -245,14 +248,10 @@ class Takuzu(Problem):
         self.actions(state)."""
         
         new_board = state.board.copy()
-
         new_board.set_number(action[0], action[1], action[2])
-
         hash_state = hash(new_board)
 
-
         if hash_state in self.visited_states:
-
             return self.visited_states[hash_state]
 
         new_state = TakuzuState(new_board)
@@ -283,7 +282,7 @@ class Takuzu(Problem):
             return np.all(np.isin(sum_col, (half, half+1))) and np.all(np.isin(sum_lines,(half, half+1)))
 
 
-    """def adjacent(self, state: TakuzuState):
+    def adjacent(self, state: TakuzuState):
         board = state.board.board
         v = np.lib.stride_tricks.sliding_window_view(board, 3, axis=1)
         v = v.reshape((v.shape[0]*v.shape[1],3)).sum(axis=1)
@@ -293,20 +292,17 @@ class Takuzu(Problem):
         v = v.reshape((v.shape[0]*v.shape[1],3)).sum(axis=1)
         cols = np.all(np.isin(v, (1, 2)))
 
-        return rows and cols"""
+        return rows and cols
 
     def goal_test(self, state: TakuzuState):
         """Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes."""
 
-        if 2 in state.board.board:
-            return False
-        else:
-            return self.half_half(state) and self.dif_rows_cols(state) #and self.adjacent(state)
-
+        return 2 not in state.board.board
+            
     
-    """def find_broken_rules(self, node: Node, board_np, i):
+    def find_broken_rules(self, node: Node, board_np, i):
         board = node.state.board
         board_size = board.board_size
 
@@ -317,15 +313,22 @@ class Takuzu(Problem):
             if np.any(board_np[indices != i, :] == board_np[i, :]):
                 return board_size**3
 
-        return 0"""
+        return 0
 
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
-    
-        board_np = node.state.board.board
+        
+        twos = np.count_nonzero(node.state.board.board == 2)
 
-        return np.count_nonzero((board_np == 2)) #f #como estava antes mas decidi meter o número de casas vazias como h p experimentar, quero fechar a árvore o mais rápido possível e tentar primeiro os estados com menos casas
+        if node.parent:
+            row_idx, col_idx,_ = node.action
+            row = np.count_nonzero(node.state.board.board[row_idx] == 2)
+            col = np.count_nonzero(node.state.board.board[:,col_idx] == 2)
+            return twos + 2*row + 2*col #para prioritizar ações em linhas com poucos 2, para dar mais peso a completar a linha
+        return twos
+
+        #return np.count_nonzero(node.state.board.board == 2) #f #como estava antes mas decidi meter o número de casas vazias como h p experimentar, quero fechar a árvore o mais rápido possível e tentar primeiro os estados com menos casas
 
 if __name__ == "__main__":
     
