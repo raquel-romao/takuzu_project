8ae4a46b254f161b59c62c3b7b6b51ffbf586f3b
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-20 17:29:22 +0100

    ver problem

--- takuzu2.py
+++ takuzu2.py
@@ -87,7 +87,7 @@ class Board:
         if (col not in (0, n-1)):
             check.append((self.get_number(row, col-1), self.get_number(row, col+1)))
 
-        print (check)
+        print(f'checkar a coluna da possível ação {check} para o valor {move}')
 
         return all(t.count(move) != 2 for t in check)
 
@@ -103,7 +103,7 @@ class Board:
         if (row not in (0, n-1)):
             check.append((self.get_number(row-1, col), self.get_number(row+1, col)))
 
-        print(check)
+        print(f'checkar a coluna da possível ação {check} para o valor {move}')
 
         return all(t.count(move) != 2 for t in check)
 
