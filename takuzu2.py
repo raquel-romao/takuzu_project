09db89d107b480617e0e16370ac3bdbf7b34d82e
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-20 17:38:39 +0100

    ver problem

--- takuzu2.py
+++ takuzu2.py
@@ -376,6 +376,8 @@ if __name__ == "__main__":
     
     board = Board.parse_instance_from_stdin()
 
+    print(board)
+
     # Criar uma instância de Takuzu:
     problem = Takuzu(board)
     # Obter o nó solução usando a procura em profundidade:
