e3d0e8ec740de5a0a3a372bcb7ed5d5988d50721
laurabquintas <laura.quintas@tecnico.ulisboa.pt>
2022-06-24 02:17:17 +0100

    messy

--- takuzu.py
+++ takuzu.py
@@ -215,6 +215,7 @@ class TakuzuState:
             actions =[]
             print(self.np_board)
             for i in empty:
+                print(self.np_board)
                 if self.board.get_number(*i)==2:
                     row_idx, col_idx = i
                     position_actions = []
@@ -332,9 +333,11 @@ class TakuzuState:
                             print('4')
 
                             if self.board.cols[i[1],0]==half-1: #quer dizer que é o 0 que apenas falta acrescentar 1
+                                print('problema com 0')
                                 self.para_colunas(i,0,test_col)
 
                             elif self.board.cols[i[1],1]==half-1: #quer dizer que é o 1 que apenas falta acrescentar 1
+                                print('problema com 1')
                                 self.para_colunas(i,1,test_col)
                                 
                         
