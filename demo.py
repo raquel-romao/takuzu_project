42c38ad07ec346ab7d39615413dcfcc52d133256
raquel-romao <raquelromao4700@gmail.com>
2022-06-25 01:18:44 +0100

    to run multiple tests

---
+++ demo.py
@@ -0,0 +1,6 @@
+import os
+
+for i in range(1,14):
+    s = f"(python C:\\Users\\UX430U\\OneDrive - Universidade de Lisboa\\Documentos\\MEBiol\\4.º ano\\2º semestre\\MINOR\\Inteligência Artificial\\AI_project\\input_T{i:02d}) > out{i:02d}"
+    os.system(s)
+  
