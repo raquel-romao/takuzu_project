44168cf22f0fc1c2cce42431f947c3059df5ec71
raquel-romao <raquelromao4700@gmail.com>
2022-06-27 20:11:55 +0100

    tirei da heuristica um if que cheguei à conclusão q n fazia mt diferença

--- takuzu_relas_copy.py
+++ takuzu_relas_copy.py
@@ -636,11 +636,8 @@ class Takuzu(Problem):
             col = np.count_nonzero(node.state.board.board[:,col_idx] == 2)
             dif = abs((node.state.board.board==0).sum() - (node.state.board.board==1).sum())
             f = twos + dif + broken_rules + row + col
-
-            if np.count_nonzero(node.state.board.board == 2) < node.state.board.board_size:
-              f = f - node.state.board.board_size
         
-        return  f
+        return  f 
 
 
 def compare_searchers(problem, header, searchers):
